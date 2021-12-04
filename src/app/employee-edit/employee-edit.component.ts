15a421d08db4621e1e9a19b7c5be52121fefad97
Gabriela Vespoli <gabiat3@hotmail.com>
2021-12-04 13:01:15 -0300

    editing employees

--- src/app/employee-edit/employee-edit.component.ts
+++ src/app/employee-edit/employee-edit.component.ts
@@ -1,15 +1,35 @@
+import { RestApiService } from './../shared/rest-api.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-employee-edit',
   templateUrl: './employee-edit.component.html',
-  styleUrls: ['./employee-edit.component.css']
+  styleUrls: ['./employee-edit.component.css'],
 })
 export class EmployeeEditComponent implements OnInit {
+  id = this.actRoute.snapshot.params['id'];
+  employeeData: any = {};
 
-  constructor() { }
+  constructor(
+    public restApi: RestApiService,
+    public actRoute: ActivatedRoute,
+    public router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.restApi.getEmployee(this.id).subscribe((data: {}) => {
+      this.employeeData = data;
+    });
   }
 
+  updateEmployee() {
+    if (window.confirm('Are you sure, you want to update?')) {
+      this.restApi
+        .updateEmployee(this.id, this.employeeData)
+        .subscribe((data) => {
+          this.router.navigate(['/employees-list']);
+        });
+    }
+  }
 }
